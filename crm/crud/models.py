b9b5401d9ef5db3e513e09d14b4d7cf4c4f99428
thefirsthouse <vasilijanenko4@gmail.com>
2025-06-09 05:18:54 +0300

    created client model

--- crm/crud/models.py
+++ crm/crud/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    full_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    birthday = models.DateField(null=True, blank=True)
+    notes = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
